75e3e490f33b3d2592fe24e11881d03465d7aae2
jinoo98 <bitnari123@naver.com>
2026-03-12 17:19:59 +0900

    pull 완료

--- config/settings.py
+++ config/settings.py
@@ -130,7 +130,7 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'frontend' / 'legacy'],
+        'DIRS': [BASE_DIR / 'frontend' / 'legacy' / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
